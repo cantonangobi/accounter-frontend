db7ad8acdbac154d9389797258652e681deed5ab
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-26 17:50:11 +0300

    Added Transaction Side bar

---
+++ src/Components/TransactionSideBar.tsx
@@ -0,0 +1,13 @@
+function TransactionSideBar() {
+	return (
+		<div
+			id="sidebar"
+			className="card p-2 h-100 h-md-auto container-fluid text-center sidebar"
+		>
+			<h1 className="m-2">Transactions</h1>
+			<button className="btn btn-success m-2 p-2">Add Transaction</button>
+		</div>
+	);
+}
+
+export default TransactionSideBar;
