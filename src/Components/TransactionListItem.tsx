0344111321d4630b6a5cd05ebee821a84ba1b0c3
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-26 16:57:15 +0300

    initialized Transaction List item component

---
+++ src/Components/TransactionListItem.tsx
@@ -0,0 +1,13 @@
+function AccountListItem() {
+	return (
+		<div className="transaction-list-item card p-4 m-2">
+			<div className="row">
+				<div className="col-4 text-start">Category</div>
+				<div className="col-4 text-start">Account</div>
+				<div className="col-4 text-start">Amount</div>
+			</div>
+		</div>
+	);
+}
+
+export default AccountListItem;
