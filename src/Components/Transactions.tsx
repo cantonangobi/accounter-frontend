1ce2fab67aaa2977d0f35bf46511066fd70acdeb
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-26 17:51:34 +0300

    added transactions component

---
+++ src/Components/Transactions.tsx
@@ -0,0 +1,19 @@
+import TransactionList from "./TransactionList";
+import TransactionSideBar from "./TransactionSideBar";
+
+function Transactions() {
+	return (
+		<main className="container-fluid h-100 ">
+			<div className="row gx-2 h-100">
+				<div className="col-md-3 p-2 h-100 h-md-auto ">
+					<TransactionSideBar />
+				</div>
+				<div className="col-md-9 p-2 min-h-100 h-md-auto">
+					<TransactionList />
+				</div>
+			</div>
+		</main>
+	);
+}
+
+export default Transactions;
