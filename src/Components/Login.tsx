7b89964b2a1523d91ca5881287cbf27321b91fb0
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-27 21:26:16 +0300

    initialized login component

---
+++ src/Components/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+	return (
+		<main className="container-fluid h-100">
+			<div className="col-6 bg-main"></div>
+			<div className="col-6"></div>
+		</main>
+	);
+}
+
+export default Login;
