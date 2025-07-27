616eb73a9168f42db7624236d43e03187b1ec61b
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-28 01:17:31 +0300

    some updates on create account component

--- src/Components/Accounts/AccountSideBar.tsx
+++ src/Components/Accounts/AccountSideBar.tsx
@@ -2,15 +2,13 @@ import CreateAccount from "./CreateAccount";
 
 function AccountSideBar() {
 	return (
-		<div
-			id="sidebar"
-			className="card p-2 h-100 h-md-auto container-fluid text-center sidebar">
+		<div className="card p-2 h-100 h-md-auto container-fluid text-center sidebar">
 			<h1 className="m-2">Accounts</h1>
 			<button
 				className="btn btn-main m-2 p-2"
 				type="button"
 				data-bs-toggle="modal"
-				data-bs-target="#exampleModal">
+				data-bs-target="#create-account-modal">
 				Add Account
 			</button>
 
--- src/Components/Accounts/CreateAccount.tsx
+++ src/Components/Accounts/CreateAccount.tsx
@@ -1,10 +1,12 @@
 function CreateAccount() {
 	return (
-		<div className="modal fade" id="exampleModal" tabIndex={-1}>
+		<div className="modal fade" id="create-account-modal" tabIndex={-1}>
 			<div className="modal-dialog modal-dialog-centered">
 				<div className="modal-content">
-					<div className="modal-header">
-						<h1 className="modal-title fs-5" id="exampleModalLabel">
+					<div className="modal-header border-0">
+						<h1
+							className="modal-title fs-3"
+							id="create-account-modal-label">
 							Create Account
 						</h1>
 						<button
@@ -14,7 +16,6 @@ function CreateAccount() {
 					</div>
 					<div className="modal-body">
 						<form className="text-start">
-							{/* <h1 className="mb-3">Log In</h1> */}
 							<div className="mb-3">
 								<label
 									htmlFor="account-name"
