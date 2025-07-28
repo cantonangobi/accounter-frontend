55efa160606a0f7a4ad4805e501175e8b3f625f9
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-28 03:06:39 +0300

    created edit transaction component

---
+++ src/Components/Transactions/EditTransaction.tsx
@@ -0,0 +1,124 @@
+function EditTransaction() {
+	return (
+		<div className="modal fade" id="create-transaction-modal" tabIndex={-1}>
+			<div className="modal-dialog modal-dialog-centered">
+				<div className="modal-content">
+					<div className="modal-header border-0">
+						<h1
+							className="modal-title fs-3"
+							id="create-transaction-modal-label"
+						>
+							Edit Transaction
+						</h1>
+						<button
+							type="button"
+							className="btn-close"
+							data-bs-dismiss="modal"
+						></button>
+					</div>
+					<div className="modal-body">
+						<form className="text-start">
+							<div className="mb-3">
+								<div className="input-group d-flex mb-3 w-100">
+									<input
+										type="radio"
+										className="btn-check invisible"
+										name="type"
+										id="expense"
+										autoComplete="off"
+									/>
+									<label
+										className="btn btn-outline-primary flex-fill"
+										htmlFor="expense"
+									>
+										Expense
+									</label>
+
+									<input
+										type="radio"
+										className="btn-check"
+										name="type"
+										id="income"
+										autoComplete="off"
+									/>
+									<label
+										className="btn btn-outline-primary flex-fill"
+										htmlFor="income"
+									>
+										Income
+									</label>
+								</div>
+
+								<div className="mb-3">
+									<label
+										htmlFor="amount"
+										className="form-label"
+									>
+										Amount
+									</label>
+									<input
+										type="number"
+										className="form-control"
+										id="amount"
+										required
+									/>
+								</div>
+								<div className="mb-3">
+									<label
+										htmlFor="select-account"
+										className="form-label"
+									>
+										Account
+									</label>
+									<select
+										className="form-select"
+										aria-label="Default select example"
+										id="select-account"
+										required
+									>
+										<option selected value="1">
+											Account 1
+										</option>
+										<option value="2">Account 2</option>
+										<option value="3">Account 3</option>
+										<option value="4">Account 4</option>
+									</select>
+								</div>
+								<div className="mb-3">
+									<label
+										htmlFor="select-account"
+										className="form-label"
+									>
+										Category
+									</label>
+									<select
+										className="form-select"
+										aria-label="Default select example"
+										id="select-account"
+										required
+									>
+										<option selected value="1">
+											Category 1
+										</option>
+										<option value="2">Category 2</option>
+										<option value="3">Category 3</option>
+										<option value="4">Category 4</option>
+									</select>
+								</div>
+							</div>
+
+							<button
+								type="submit"
+								className="btn btn-main w-100"
+							>
+								Save
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default EditTransaction;
