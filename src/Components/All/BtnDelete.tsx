a8cc612ebc402c086f5fe1db513047a085116488
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-28 03:18:35 +0300

    created confirm delete modal

--- src/Components/All/BtnDelete.tsx
+++ src/Components/All/BtnDelete.tsx
@@ -1,8 +1,18 @@
+import ConfirmDelete from "./ConfirmDelete";
+
 function BtnDelete() {
 	return (
-		<button className="btn btn-danger py-1 px-2 mx-tiny">
-			<i className="fa-solid fa-trash"></i>
-		</button>
+		<>
+			<button
+				className="btn btn-danger py-1 px-2 mx-tiny"
+				type="button"
+				data-bs-toggle="modal"
+				data-bs-target="#confirm-delete"
+			>
+				<i className="fa-solid fa-trash"></i>
+			</button>
+			<ConfirmDelete />
+		</>
 	);
 }
 
---
+++ src/Components/All/ConfirmDelete.tsx
@@ -0,0 +1,39 @@
+function ConfirmDelete() {
+	return (
+		<div className="modal fade" id="confirm-delete" tabIndex={-1}>
+			<div className="modal-dialog modal-dialog-centered">
+				<div className="modal-content">
+					<div className="modal-header">
+						<h5 className="modal-title">Modal title</h5>
+						<button
+							type="button"
+							className="btn-close"
+							data-bs-dismiss="modal"
+							aria-label="Close"
+						></button>
+					</div>
+					<div className="modal-body">
+						<p>
+							Are you sure you want to Delete? This action is
+							irreversible.
+						</p>
+					</div>
+					<div className="modal-footer">
+						<button
+							type="button"
+							className="btn btn-secondary"
+							data-bs-dismiss="modal"
+						>
+							Cancel
+						</button>
+						<button type="button" className="btn btn-danger">
+							Delete
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ConfirmDelete;
