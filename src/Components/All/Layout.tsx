dbbf60dc027d60a955cc37a4431eb79ba3d7d05b
Cantona Ngobi <cantonangobi@gmail.com>
2025-10-08 20:47:26 +0300

    added hook in layout to check if user is logged in, if not, they are redirected to login page

--- src/Components/All/Layout.tsx
+++ src/Components/All/Layout.tsx
@@ -1,7 +1,54 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import Header from "./Header";
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Security/AuthProvider";
+const BASE_URL = "http://localhost:8080/api/v1/auth/test";
 
 function Layout() {
+	const authContext = useContext(AuthContext);
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		console.log("Auth testing...");
+		console.log(authContext?.auth.token);
+		if (!authContext?.auth.token) {
+			navigate("/login", {
+				state: { from: location },
+				replace: true,
+			});
+			// return;
+		}
+
+		axios
+			.get(BASE_URL, {
+				headers: { Authorization: authContext?.auth.token },
+			})
+			.then((response) => {
+				console.log(`Server Status: ${response.status}`);
+				console.log(`data: ${response.data}`);
+				if (response.status !== 200) {
+					// 	tokenValid = true;
+					// 	console.log("Token is valid");
+					// } else {
+					// tokenValid = false;
+					navigate("/login", {
+						state: { from: location },
+						replace: true,
+					});
+				}
+			})
+			.catch((error) => {
+				console.error(`Server Error: ${error}`);
+				// tokenValid = false;
+				navigate("/login", {
+					state: { from: location },
+					replace: true,
+				});
+			});
+	}, []);
+
 	return (
 		<>
 			<Header />
