d5e4fff605e96304942edea3367f5d59e3b67b74
Cantona Ngobi <cantonangobi@gmail.com>
2025-10-20 15:26:35 +0300

    created session management component to provide session management functions

---
+++ src/Components/Security/SessionManagement.tsx
@@ -0,0 +1,19 @@
+export function setSessionUser(user: any) {
+	window.sessionStorage.setItem("sessionUser", JSON.stringify(user));
+}
+
+export function setSessionToken(jwtToken: string) {
+	window.sessionStorage.setItem("sessionToken", `Bearer ${jwtToken}`);
+}
+
+export function getSessionUser() {
+	return window.sessionStorage.getItem("sessionUser");
+}
+
+export function getSessionToken() {
+	return window.sessionStorage.getItem("sessionToken");
+}
+
+export function resetSession() {
+	window.sessionStorage.clear();
+}
